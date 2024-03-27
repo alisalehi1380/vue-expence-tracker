772454266a4139482d0a19749cd94fb4823ff7b1
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-27 17:30:48 +0330

    add AppInput component

---
+++ src/components/global/AppInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+      style="font-size: 0.75rem"
+      class="rounded text-sm"
+      :type="type"
+      :placeholder="placeholder"
+  />
+  <!--v-model="amount"-->
+</template>
+
+<script setup lang="ts">
+import {defineProps, withDefaults} from 'vue';
+
+interface Props {
+  type?: "text" | "number";
+  placeholder: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  type: "text"
+});
+</script>
