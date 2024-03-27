f5e0de42f2f9b42c17512f8f2a5a6d97a62834ef
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-28 01:02:52 +0330

    add customClass in TomanIcon

--- src/components/global/icons/TomanIcon.vue
+++ src/components/global/icons/TomanIcon.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="flex items-center" dir="ltr">
     <span class="price-label"></span>
-    <span class="sample_farsi_digits">{{ amount }}</span>
+    <span
+        class="sample_farsi_digits"
+        :class="customClass"
+    >{{ absoluteNumber }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
 import {defineProps} from 'vue';
 
+
 interface Props {
   amount: number
+  customClass?: string
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+const absoluteNumber = Math.abs(props.amount);
 </script>
