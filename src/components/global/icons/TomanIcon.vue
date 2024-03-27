6e4b1c34cc549bbf6ca6967c728fdc2c356bf1bd
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-28 00:23:32 +0330

    add TomanIcon component

---
+++ src/components/global/icons/TomanIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center" dir="ltr">
+    <span class="price-label"></span>
+    <span class="sample_farsi_digits">{{ amount }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+interface Props {
+  amount: number
+}
+
+defineProps<Props>();
+</script>
