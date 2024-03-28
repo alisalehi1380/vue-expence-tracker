bef9dc2a805ca11ee88bef6b83f234983211d7d6
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-28 17:25:55 +0330

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import {createApp} from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import {createPinia} from 'pinia';
+import App from './App.vue';
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(Toast);
-app.mount('#app')
+app.mount('#app');
 
