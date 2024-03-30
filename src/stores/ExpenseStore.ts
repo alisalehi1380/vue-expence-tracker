7516d05635be33a00c28bba00013087f64637cd8
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-30 15:50:28 +0330

    add fetchExpenses & deleteExpense

--- src/stores/ExpenseStore.ts
+++ src/stores/ExpenseStore.ts
@@ -1,24 +1,34 @@
-import {defineStore} from "pinia";
 import {ref} from "vue";
+import {defineStore} from "pinia";
+import {useToast} from 'vue-toastification';
+import {useApi} from "../composables/useApi";
 
 export const useExpenseStore = defineStore('expense', () => {
+    const toast = useToast();
     const expenseList = ref([] as ITransaction[]);
     const expenseItem = ref({} as ITransaction);
 
-    const fetchExpenses = (data: ITransaction[]) => {
-        // expenseList.value = data.value as ITransaction[];
-        expenseList.value = data
-        // console.log(data)
-        // console.log(expenseList.value)
-        // console.log(expenseItem.value)
+    // @ts-ignore
+    const fetchExpenses = async () => {
+        const {data, error} = await useApi("/todo");
+        if (data.value) expenseList.value = data.value as ITransaction[];
     };
 
     const createExpense = (expense) => {
-        // console.log(expense)
-        // console.log(expenseList.value)
-        if (expense) expenseList.value.push(expense)
+        // if (expense) expenseList.value.push(expense)
     };
 
+    // @ts-ignore
+    const deleteExpense = async (id: number) => {
+        const {data, error} = await useApi(`/todo/${id}`).delete();
+        if (data.value) {
+            const foundIndex = expenseList.value.findIndex((x) => x.id === id);
+            expenseList.value.splice(foundIndex, 1);
+            toast.success('حذف با موفقیت انجام شد');
+        } else {
+            toast.error('متاسفانه مشکلی رخ داده است');
+        }
+    }
 
-    return {fetchExpenses, createExpense,expenseList};
+    return {expenseList, expenseItem, fetchExpenses, createExpense, deleteExpense};
 });
