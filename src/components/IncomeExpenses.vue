3d580c4e5e678663d134864427009a1e4c38fc4d
Ali Salehi <alisalehi13800930@gmail.com>
2024-03-31 01:38:27 +0330

    refactor IncomeExpenses

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -1,6 +1,4 @@
 <template>
-  <h2 class="text-center mb-4 font-semibold text-xl">حسابگر هزینه</h2>
-  <hr class="border-b border-b-gray-50 mb-4" />
   <div class="flex shadow mb-4 justify-evenly py-4 text-center">
     <div>
       <h4 class="mb-1 text-sm">درآمد</h4>
@@ -9,19 +7,23 @@
     <div class="h-max-2 bg-gray-200 w-[1px]"></div>
     <div>
       <h4 class="mb-1 text-sm">مخارج</h4>
-      <toman-icon :amount="expenses" customClass="text-red-600" />
+      <toman-icon :amount="expense" customClass="text-red-600" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {defineProps} from 'vue';
+import {computed} from 'vue';
 import TomanIcon from "@/components/global/icons/TomanIcon.vue";
+import {useExpenseStore} from "@/stores/ExpenseStore";
 
-interface Props {
-  income: number
-  expenses: number
-}
+const useExpense = useExpenseStore();
 
-defineProps<Props>();
+const income = computed(() => {
+  return useExpense.totalIncome();
+});
+const expense = computed(() => {
+  return useExpense.totalExpense();
+});
+
 </script>
