ff60082987c866a8f47cadb1937a7ced350c8ed7
Ali Salehi <alisalehi13800930@gmail.com>
2024-04-01 15:10:17 +0330

    refactor AddTransaction component

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,60 +1,45 @@
 <template>
   <h3 class="mb-1 text-sm font-medium dark:text-stone-300">ایجاد تراکنش</h3>
-  <form @submit.prevent="onSubmit">
+  <div>
     <div class="form-control mb-4 space-y-2">
       <app-input
           placeholder="عنوان تراکنش را وارد کنید"
           type="text"
+          v-model="expenseStore.expenseItem.title"
       />
       <app-input
           placeholder="مبلغ را وارد کنید"
           type="number"
+          v-model="expenseStore.expenseItem.amount"
       />
     </div>
-    <button
-        class="p-2 w-full h-full rounded
-        bg-slate-100
-        hover:bg-slate-200
-        dark:text-stone-300
-        dark:bg-slate-500
-        dark:hover:bg-slate-600"
-    >
+    <app-button variant="primary" @click="onSubmit">
       ثبت
-    </button>
-  </form>
+    </app-button>
+  </div>
 </template>
 
 <script setup>
-import {ref, defineEmits} from 'vue';
 import {useToast} from 'vue-toastification';
+import {useExpenseStore} from "@/stores/ExpenseStore";
 import AppInput from "@/components/global/AppInput.vue";
+import AppButton from "@/components/global/AppButton.vue";
 
-const text = ref('');
-const amount = ref('');
 const toast = useToast();
-const emit = defineEmits(['transactionSubmitted', 'handleAmountUpdate', 'handleTitleUpdate']);
+const expenseStore = useExpenseStore();
 
-const handleTitleUpdate = (newTitle) => {
-  text.value = newTitle;
-};
-const handleAmountUpdate = (newAmount) => {
-  amount.value = newAmount;
-};
-
-const onSubmit = () => {
-  if (!text.value || !amount.value) {
-    toast.error('Both fields must be filled.');
-    return;
+const onSubmit = async () => {
+  if (!expenseStore.expenseItem.title || !expenseStore.expenseItem.amount) {
+    toast.error('وارد کردن هر دو فیلد الزامی ست');
+  } else {
+    try {
+      await expenseStore.createExpense(expenseStore.expenseItem);
+      toast.success('با موفقیت ایجاد شد');
+    } catch (e) {
+      toast.error('مشکلی به وجود آمده است');
+    }
+    expenseStore.expenseItem.title = '';
+    expenseStore.expenseItem.amount = '';
   }
-
-  const transactionData = {
-    text: text.value,
-    amount: parseFloat(amount.value),
-  };
-
-  emit('transactionSubmitted', transactionData);
-
-  text.value = '';
-  amount.value = '';
 };
 </script>
